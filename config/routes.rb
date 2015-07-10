305d2eea735f25a6227e2635420dac59f0bb8660
aaronliew <aaron@favoritemedium.com>
2015-07-10 10:46:14 +0800

    customize the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/contact'
 
-  get 'pages/about'
+  root :to => 'pages#home'
+
+  get "users/new"
+  get '/signup',  :to => 'users#new'
+  get '/contact', :to => 'pages#contact' 
+  get '/about', :to => 'pages#about' 
+  get '/help', :to => 'pages#help'
+
+
+
+  # get 'pages/home'
+
+  # get 'pages/contact'
+
+  # get 'pages/about'
+
+  # get 'pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
